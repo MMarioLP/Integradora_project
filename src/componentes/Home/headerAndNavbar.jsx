e1cafd9a6f01ecdcc295f9b44368272f4ea87123
MMarioLP <am4063228@gmail.com>
2023-03-08 15:28:58 -0600

    first commit

---
+++ src/componentes/Home/headerAndNavbar.jsx
@@ -0,0 +1,113 @@
+import React, { useState,useContext} from "react";
+import "../../estilos/Home/headerAndNavbar.css";
+import { Link } from "react-router-dom";
+import {langContext} from '../../context/langContext'
+import es from '../../imagenes/spain.png';
+import en from '../../imagenes/united-kingdom.png';
+import { FormattedMessage } from "react-intl";
+
+
+
+
+export function Navbar() {
+ 
+ 
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
+
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
+  return (
+   
+    <nav className="nav">
+      <a className="nav__brand cursor">
+        <img
+          src={require("../../imagenes/logos/logo-optimen.png")}
+          alt="Logo de la empresa"
+          href="#"
+        />
+      </a>
+      <ul className={active} style={{margin:"0"}}>
+        <li className="nav__item">
+          <Link to="/Home" className="nav__link">
+          <FormattedMessage
+          id="app.NH"
+          defaultMessage="Home"
+          />
+           
+          </Link>
+        </li>
+        <li className="nav__item">
+          <Link to="/Customers" className="nav__link">
+          <FormattedMessage
+          id="app.NC"
+          defaultMessage="Customers"
+          />
+           
+          </Link>
+        </li>
+        <li className="nav__item">
+          <Link to="/About-Us" className="nav__link">
+          <FormattedMessage
+          id="app.NA"
+          defaultMessage="About us"
+          />
+            
+          </Link>
+        </li>
+        <li className="nav__item">
+        <Link to="/News" className="nav__link">
+        <FormattedMessage
+          id="app.NE"
+          defaultMessage="News"
+          />
+            
+            </Link>
+        </li>
+        <li className="nav__item">
+        <Link to="/Contact-Us" className="nav__link">
+        <FormattedMessage
+          id="app.NCU"
+          defaultMessage="Contact us"
+          />
+            
+            </Link>
+        </li>
+      </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
+   
+  );
+}
+
+export function Footer() {
+  const idioma = useContext(langContext);
+  return (
+
+    
+    <footer>
+
+      <p1>Optimen</p1>
+
+      <p>info@mysite.com</p>
+
+      <p>&copy;2023 por Optimen. </p>
+
+      <div className="banderas">
+					<button onClick={() => idioma.establecerLenguaje('es-MX')}><img src={es} alt=""/></button>
+					<button onClick={() => idioma.establecerLenguaje('en-US')}><img src={en} alt=""/></button>
+				</div>
+    </footer>
+  );
+}
